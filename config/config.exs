e9982cbd688fde8a50fdb5ee6eb5d710b130b804
spaenleh <spaenleh@gmail.com>
2025-09-13 12:59:07 +0200

    setup 2020 day 01

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,10 @@
 import Config
 
 config :aoc,
-  # The prefix to use when creating solutions and test modules with `mix aoc.create`.
+  # The prefix is used when creating solutions and test modules with `mix aoc.create`.
   prefix: AocElixir.Solutions,
-  generate_comments: false
+
+  # Optional
+
+  # Include help comments when generating modules and tests
+  generate_comments: true
