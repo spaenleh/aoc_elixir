fc03a0f52c059e183c991a51ec4e75a37d43626f
spaenleh <spaenleh@gmail.com>
2025-12-01 09:20:01 +0100

    done with part 1 but stuck on part 2

--- lib/aoc_elixir/solutions/y25/day01.ex
+++ lib/aoc_elixir/solutions/y25/day01.ex
@@ -1,15 +1,102 @@
 defmodule AocElixir.Solutions.Y25.Day01 do
   alias AoC.Input
 
+  def to_struct(line) do
+    case line do
+      "L" <> value -> -1 * String.to_integer(value)
+      "R" <> value -> String.to_integer(value)
+      _ -> 0
+    end
+  end
+
   def parse(input, _part) do
-    Input.read!(input)
+    Input.stream!(input, trim: true)
+    |> Stream.map(&to_struct/1)
   end
 
   def part_one(problem) do
     problem
+    |> Enum.reduce(%{zero: 0, pos: 50}, fn value, acc ->
+      new_pos =
+        Integer.mod(acc.pos + value, 100)
+
+      # check if stopped on 0
+      num_zero =
+        if new_pos == 0 do
+          acc.zero + 1
+        else
+          acc.zero
+        end
+
+      %{pos: new_pos, zero: num_zero}
+    end)
+    |> then(fn %{zero: num_zero} -> num_zero end)
   end
 
-  # def part_two(problem) do
-  #   problem
-  # end
+  def part_two(problem) do
+    problem
+    |> Enum.reduce(%{zero: 0, pos: 50}, fn value, acc ->
+      # IO.puts("---\npos: #{acc.pos}, value: #{value}")
+
+      new_pos =
+        Integer.mod(acc.pos + value, 100)
+
+      # this is the number of full turns that it can make
+      full_turns = div(abs(value), 100)
+      # |> IO.inspect(label: "will_turn")
+      # is the value a complement to the current pos -> we end on zero
+      value_rem = rem(value, 100)
+      pos_complement = acc.pos + value_rem
+
+      is_complement =
+        if acc.pos != 0 and (pos_complement == 0 or pos_complement == 100) do
+          1
+        else
+          0
+        end
+
+      num_zero = acc.zero + full_turns + is_complement
+      IO.puts(num_zero)
+
+      # num_zero = acc.zero
+      # check if stopped on 0
+      # num_zero =
+      #   if new_pos == 0 do
+      #     # IO.puts("Stops on 0")
+      #     acc.zero + 1
+      #   else
+      #     acc.zero
+      #   end
+
+      # if the value is negative, we crossed 0
+      # num_zero =
+      #   if acc.pos > 0 and acc.pos + value < 0 do
+      #     # IO.puts("goes past zero from + to -")
+      #     x_times = abs(div(value, 100)) + 1
+      #     # IO.puts("for #{x_times} times")
+      #     num_zero + x_times
+      #   else
+      #     num_zero
+      #   end
+      #
+      # num_zero =
+      #   if acc.pos != 0 and acc.pos + value > 100 do
+      #     num_zero + 1 + div(value, 100)
+      #   else
+      #     num_zero
+      #   end
+      #
+      # # special case for when we are on zero, we do not want to count more than needed
+      # num_zero =
+      #   if acc.pos == 0 do
+      #     num_zero + abs(div(value, 100)) + 1
+      #   else
+      #     num_zero
+      #   end
+
+      # IO.puts("num_zero: #{num_zero}")
+      %{pos: new_pos, zero: num_zero}
+    end)
+    |> then(fn %{zero: num_zero} -> num_zero end)
+  end
 end
--- test/2025/day01_test.exs
+++ test/2025/day01_test.exs
@@ -29,24 +29,145 @@ defmodule AocElixir.Solutions.Y25.Day01Test do
     assert 3 == solve(input, :part_one)
   end
 
-  # @part_one_solution CHANGE_ME
-  #
-  # test "part one solution" do
-  #   assert {:ok, @part_one_solution} == AoC.run(2025, 1, :part_one)
-  # end
+  @part_one_solution 969
+  @tag :solution
+  test "part one solution" do
+    assert {:ok, @part_one_solution} == AoC.run(2025, 1, :part_one)
+  end
 
-  # test "part two example" do
-  #   input = ~S"""
-  #   This is an
-  #   example input.
-  #   replace with
-  #   an example from
-  #   the AoC website.
-  #   """
-  #
-  #   assert CHANGE_ME == solve(input, :part_two)
-  # end
+  test "part two example" do
+    input = ~S"""
+    L68
+    L30
+    R48
+    L5
+    R60
+    L55
+    L1
+    L99
+    R14
+    L82
+    """
 
+    assert 6 == solve(input, :part_two)
+  end
+
+  test "part two double crossing in positive" do
+    input = ~S"""
+    L68
+    L30
+    R48
+    L5
+    R160
+    L55
+    L1
+    L99
+    R14
+    L82
+    """
+
+    assert 7 == solve(input, :part_two)
+  end
+
+  test "part two double zero" do
+    input = ~S"""
+    L68
+    L30
+    R48
+    R200
+    L5
+    R160
+    L55
+    L1
+    L99
+    R14
+    L82
+    """
+
+    assert 9 == solve(input, :part_two)
+  end
+
+  test "part two zero stay" do
+    input = ~S"""
+    L50
+    L200
+    """
+
+    assert 3 == solve(input, :part_two)
+  end
+
+  test "part two zero and a little more" do
+    input = ~S"""
+    L50
+    L201
+    """
+
+    assert 3 == solve(input, :part_two)
+  end
+
+  test "unit positive complement" do
+    # lands on 0 via complement
+    assert 1 ==
+             solve(
+               ~S"""
+               R50
+               """,
+               :part_two
+             )
+
+    # full turn and lands on zero
+    assert 2 ==
+             solve(
+               ~S"""
+               R150
+               """,
+               :part_two
+             )
+
+    # add a complete turn in oposite  
+    assert 3 ==
+             solve(
+               ~S"""
+               R150
+               L100
+               """,
+               :part_two
+             )
+
+    assert 4 ==
+             solve(
+               ~S"""
+               R150
+               L200
+               """,
+               :part_two
+             )
+
+    assert 4 ==
+             solve(
+               ~S"""
+               R150
+               L1
+               L199
+               """,
+               :part_two
+             )
+
+    assert 4 ==
+             solve(
+               ~S"""
+               R151
+               L1
+               L200
+               """,
+               :part_two
+             )
+  end
+
+  # 59xx too high
+  # 5767 too low
+  # 5869 not correct
+  #
   # @part_two_solution CHANGE_ME
   #
   # test "part two solution" do
