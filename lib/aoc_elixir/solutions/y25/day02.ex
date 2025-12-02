7aef9228f92950071a6bc0b49d22787cb8339a72
spaenleh <spaenleh@gmail.com>
2025-12-02 06:50:20 +0100

    nearly there

---
+++ lib/aoc_elixir/solutions/y25/day02.ex
@@ -0,0 +1,47 @@
+defmodule AocElixir.Solutions.Y25.Day02 do
+  alias AoC.Input
+
+  def parse(input, _part) do
+    Input.read!(input)
+    |> String.trim()
+    |> String.split(",")
+  end
+
+  def part_one(problem) do
+    problem
+    |> Enum.map(&String.split(&1, "-"))
+    # |> IO.inspect()
+    |> Enum.map(fn [start_val, end_val] ->
+      String.to_integer(start_val)..String.to_integer(end_val)
+      |> Enum.reject(fn id ->
+        # ID that is even can not be fake
+        id_string = Integer.to_charlist(id)
+        id_length = length(id_string)
+        # IO.puts("#{id_length}, #{mid}, #{start_string}, #{end_string}")
+
+        case id do
+          _ when id < 11 ->
+            true
+
+          _ when rem(id_length, 2) != 0 ->
+            # IO.puts("odd size")
+            true
+
+          _ ->
+            mid = div(id_length, 2)
+            start_string = String.slice("#{id_string}", 0..(mid - 1))
+            end_string = String.slice("#{id_string}", mid..-1//1)
+
+            !start_string == end_string
+        end
+      end)
+    end)
+    |> Enum.flat_map(& &1)
+    # |> Enum.map(&IO.inspect(&1))
+    |> Enum.sum()
+  end
+
+  # def part_two(problem) do
+  #   problem
+  # end
+end
---
+++ test/2025/day02_test.exs
@@ -0,0 +1,46 @@
+defmodule AocElixir.Solutions.Y25.Day02Test do
+  alias AoC.Input, warn: false
+  alias AocElixir.Solutions.Y25.Day02, as: Solution, warn: false
+  use ExUnit.Case, async: true
+
+  defp solve(input, part) do
+    problem =
+      input
+      |> Input.as_file()
+      |> Solution.parse(part)
+
+    apply(Solution, part, [problem])
+  end
+
+  test "part one example" do
+    input = ~S"""
+    11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124
+    """
+
+    assert 1_227_775_554 == solve(input, :part_one)
+  end
+
+  # @part_one_solution CHANGE_ME
+  #
+  # test "part one solution" do
+  #   assert {:ok, @part_one_solution} == AoC.run(2025, 2, :part_one)
+  # end
+
+  # test "part two example" do
+  #   input = ~S"""
+  #   This is an
+  #   example input.
+  #   replace with
+  #   an example from
+  #   the AoC website.
+  #   """
+  #
+  #   assert CHANGE_ME == solve(input, :part_two)
+  # end
+
+  # @part_two_solution CHANGE_ME
+  #
+  # test "part two solution" do
+  #   assert {:ok, @part_two_solution} == AoC.run(2025, 2, :part_two)
+  # end
+end
