8335b77200f0183028b5b17b584240cf319f13ab
spaenleh <spaenleh@gmail.com>
2025-12-02 17:08:32 +0100

    fix: part 2

--- test/2025/day02_test.exs
+++ test/2025/day02_test.exs
@@ -26,13 +26,13 @@ defmodule AocElixir.Solutions.Y25.Day02Test do
     assert {:ok, @part_one_solution} == AoC.run(2025, 2, :part_one)
   end
 
-  # test "part two example" do
-  #   input = ~S"""
-  #   11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124
-  #   """
-  #
-  #   assert 4_174_379_265 == solve(input, :part_two)
-  # end
+  test "part two example" do
+    input = ~S"""
+    11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124
+    """
+
+    assert 4_174_379_265 == solve(input, :part_two)
+  end
 
   # @part_two_solution CHANGE_ME
   #
